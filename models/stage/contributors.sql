5eaa4594feecf6e604c73646718fb116b33044cc
Stewart Bryson <stewart@redpillanalytics.com>
2020-04-07 05:54:00 +0000

    Corrected concat test.

--- models/stage/contributors.sql
+++ models/stage/contributors.sql
@@ -1,14 +1,12 @@
-select distinct 
-  contributor_name,
-  contributor_email
-from (
-    select
-    author_name contributor_name,
-    author_email contributor_email
-    from {{ref('commits')}}
-    UNION DISTINCT
-    select 
-    committer_name,
-    committer_email
-    from {{ref('commits')}}
-)
+
+SELECT
+  DISTINCT author_name contributor_name,
+  author_email contributor_email
+FROM
+  {{ref('commits')}}
+UNION DISTINCT
+SELECT
+  DISTINCT committer_name,
+  committer_email
+FROM
+  {{ref('commits')}}
